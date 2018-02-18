1a4b4340c0e133162a76be171bc267334c1c581c
olmancp <olmancp95@gmail.com>
2018-02-17 19:43:15 -0600

    website 0.1.2
    back end

---
+++ website/main/back_end/control/Facade.py
@@ -0,0 +1,21 @@
+class Facade:
+    
+    def __init__(self):
+        x=0
+    
+    def cargar_imagen(self):
+        return True
+    def estimar_edad(self):
+        return True
+    def desplegar_edad(self):
+        return 0
+    def guardar_informacion_paciente(self):
+        return True
+    def cargar_imagenes(self):
+        return True
+    def cargar_cvs(self):
+        return True
+    def calcular_MAE(self):
+        return True
+    def calcular_MSE(self):
+        return True
---
+++ website/main/back_end/control/FacedeMedico.py
@@ -0,0 +1,13 @@
+class FacadeMedico:
+    
+    def __init__(self):
+        x=0
+    
+    def cargar_imagen(self):
+        return True
+    def estimar_edad(self):
+        return True
+    def desplegar_edad(self):
+        return 0
+    def guardar_informacion_paciente(self):
+        return True
