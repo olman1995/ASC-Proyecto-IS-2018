13a7575bdc3b788ce005ced6a1028291e021d23e
olmancp <olmancp95@gmail.com>
2018-02-19 17:24:12 -0600

    website 0.1.7

--- website/main/models.py
+++ website/main/models.py
@@ -7,3 +7,19 @@ class usuario(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+    
+class paciente(models.Model):
+    edad = models.IntegerField()
+    estimacion_edad =  models.IntegerField()
+    url_imagen = models.CharField(max_length=200)
+    nombre =models.CharField(max_length=20)
+    apellido_1 =models.CharField(max_length=20)
+    apellido_2 =models.CharField(max_length=20)
+    cedula = models.IntegerField()
+    hospital = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
